661f5cb9dce53d0b057ff13ce1b48cba40968aab
imtechgeek <usmanirfan996@gmail.com>
2025-10-16 14:40:02 +0200

    refactor: update app and routing structure

--- be/main.py
+++ be/main.py
@@ -1,86 +1,31 @@
-from typing import Annotated, Any, Union
-from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, SQLModel, Session, create_engine, select
+from contextlib import asynccontextmanager
+from typing import Annotated
+from fastapi import Depends, FastAPI
+from sqlmodel import Session
+from fastapi.middleware.cors import CORSMiddleware
+from database import create_db_and_tables, get_session
+from routers import questions, answers
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+    
+    
+app = FastAPI(lifespan=lifespan)
 
-class Question(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    text: str = Field(index=True)
+app.include_router(questions.router)
+app.include_router(answers.router)
 
-class Answer(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    question_id: int = Field(foreign_key="question.id")
-    text: str = Field(index=True)
-
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-
-def get_session():
-    with Session(engine) as session:
-        yield session
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex='http://localhost:\d+$',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
-
-@app.post("/question/")
-def create_question(question: Question, session: SessionDep) -> Question:
-    session.add(question)
-    session.commit()
-    session.refresh(question)
-    return question
 
 
-@app.get("/question/")
-def read_questions(
-    session: SessionDep,
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Question]:
-    question = session.exec(select(Question).offset(offset).limit(limit)).all()
-    return [*question]
-
-
-@app.get("/question/{question_id}")
-def read_question(question_id: int, session: SessionDep) -> Question:
-    question = session.get(Question, question_id)
-    if not question:
-        raise HTTPException(status_code=404, detail="question not found")
-    return question
-
-
-@app.post("/answer/")
-def create_answer(answer: Answer, session: SessionDep) -> Answer:
-    session.add(answer)
-    session.commit()
-    session.refresh(answer)
-    return answer
-
-
-@app.get("/answer/")
-def read_answers(
-    session: SessionDep,
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Answer]:
-    answer = session.exec(select(Answer).offset(offset).limit(limit)).all()
-    return [*answer]
-
-
-@app.get("/answer/{answer_id}")
-def read_answer(answer_id: int, session: SessionDep) -> Answer:
-    answer = session.get(Answer, answer_id)
-    if not answer:
-        raise HTTPException(status_code=200, detail="answer not found")
-    return answer
---
+++ be/routers/answers.py
@@ -0,0 +1,26 @@
+from typing import Annotated
+from fastapi import Depends, APIRouter, HTTPException, Path, Query
+from sqlmodel import Session, select
+
+from models import Answer, Question
+from schemas import AnswerCreate
+from database import get_session
+from starlette import status
+
+router = APIRouter(
+    tags=['answer']
+)
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+@router.post("/answer/", status_code=status.HTTP_201_CREATED)
+def create_answer(answer: AnswerCreate, session: SessionDep) -> Answer:
+    question = session.exec(select(Question).where(answer.question_id == Question.id)).first()
+    if not question:
+        raise HTTPException(status_code=401, detail='no question found for the provided question id')
+    
+    db_answer = Answer.model_validate(answer)
+    session.add(db_answer)
+    session.commit()
+    session.refresh(db_answer)
+    return db_answer
