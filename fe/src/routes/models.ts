5649b03701a9cc9cde4e2dbba834e34214013838
imtechgeek <usmanirfan996@gmail.com>
2025-10-16 14:44:15 +0200

    feat: update models

--- fe/src/routes/models.ts
+++ fe/src/routes/models.ts
@@ -1,10 +1,15 @@
-interface Question {
-    id: number
-    text: string
+export interface Question {
+	id: number;
+	text: string;
+	views: number;
 }
 
-interface Answer {
-    id: number
-    text: string
-    question_id: number
+export interface Answer {
+	id: number;
+	text: string;
+	question_id: number;
+}
+export interface AnswersWithQuestion {
+	question_text: string;
+	answers: Answer[];
 }
