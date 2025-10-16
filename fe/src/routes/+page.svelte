45df03e81423839d97eb464579b1677dfc92e20b
imtechgeek <usmanirfan996@gmail.com>
2025-10-16 14:44:39 +0200

    feat: add new views route and remove unused code

--- fe/src/routes/+page.svelte
+++ fe/src/routes/+page.svelte
@@ -1,6 +1,5 @@
-<script lang="ts">
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcomeFallback from '$lib/images/svelte-welcome.png';
+<script>
+	import Container from '$lib/components/Container.svelte';
 </script>
 
 <svelte:head>
@@ -8,36 +7,23 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-
-</section>
+<Container alignCenter>
+	<h1>Welcome 👋</h1>
+	<p>Find and ask questions from the community!</p>
+	<a href="/questions" class="button">Go to Questions</a>
+</Container>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+	.button {
+		display: inline-block;
+		margin-top: 1rem;
+		padding: 0.5rem 1rem;
+		background: #0070f3;
+		color: white;
+		text-decoration: none;
+		border-radius: 4px;
 	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.button:hover {
+		background: #005ac1;
 	}
 </style>
